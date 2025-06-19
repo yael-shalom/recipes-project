849052b760c0b83b11654a932dd7c44fac59109d
yael-shalom <yael7147757@gmail.com>
2025-06-19 23:02:45 +0300

    change BrowserRouter to HashRouter

--- react-client/src/main.jsx
+++ react-client/src/main.jsx
@@ -3,15 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './features/common/store'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './features/common/App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
